ccfa2cfffb82c6cd1e1038e891b7a65cc0c61706
Jack Oliveira <jackoliveira14adm10@gmail.com>
2022-08-04 21:37:28 -0300

    04-08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-<<<<<<< HEAD
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -9,15 +8,6 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     HomeComponent,
     HeaderComponent
-=======
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
->>>>>>> 722b4b8c71e578a25435d96a5000ba581a9a812b
   ],
   imports: [
     BrowserModule
